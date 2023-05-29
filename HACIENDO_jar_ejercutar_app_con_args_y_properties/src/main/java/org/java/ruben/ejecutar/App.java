49a15a0e699fd53c150d63e56c837d8d4b07a658
Ruben <info@blogruben.com>
2023-05-29 14:29:02 +0200

    haciendo jar ejercutar appp args y propiedades

--- HACIENDO_jar_ejercutar_app_con_args_y_properties/src/main/java/org/java/ruben/ejecutar/App.java
+++ HACIENDO_jar_ejercutar_app_con_args_y_properties/src/main/java/org/java/ruben/ejecutar/App.java
@@ -7,8 +7,9 @@ public class App {
     public static void main(String[] args)  {
         
         System.out.print("Los argumentos son -> ");
-        Arrays.stream(args).forEach(str -> System.out.print(str + " "));
+        Arrays.stream(args).forEach(str -> System.out.print(str + ", "));
 
+        System.out.println(System.lineSeparator()+"La propiedad myvariable -> "+System.getProperty("myvariable"));
         System.out.println(System.lineSeparator()+"La propiedad hola -> "+System.getProperty("hola"));
 
 
