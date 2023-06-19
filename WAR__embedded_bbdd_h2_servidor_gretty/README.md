d41e301095098972cddd1c16b79a763fd4c222ae
Ruben <info@blogruben.com>
2023-06-19 12:29:54 +0200

    WAR__embedded_bbdd_h2_servidor_gretty

---
+++ WAR__embedded_bbdd_h2_servidor_gretty/README.md
@@ -0,0 +1,7 @@
+
+# Ejecutar
+4. arrancar servidor 
+``gradle appStart``
+
+ctrl+C para cerrar jetty
+
---
+++ WAR__embedded_bbdd_h2_servidor_gretty/src/main/java/web/InsertarUsuario.java
@@ -0,0 +1,64 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+@WebServlet("/addUser")
+public class InsertarUsuario extends HttpServlet {
+    
+
+	/*
+	 * http://localhost:8080/addUser?name=Raul
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException    {
+		String nombre = req.getParameter("name");
+
+        final String crearTabla = "CREATE TABLE IF NOT EXISTS user(id MEDIUMINT NOT NULL AUTO_INCREMENT ,nombre varchar(255))";
+        final String insertarUser = "INSERT INTO user(nombre) VALUES(?)";
+      
+        try(Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "", "");
+        PreparedStatement pstmtCreate = conn.prepareStatement(crearTabla);
+        PreparedStatement pstmtInsert = conn.prepareStatement(insertarUser)) {
+        
+        Class.forName("org.h2.Driver");
+        pstmtCreate.execute();
+
+        pstmtInsert.setString(1, nombre);
+        pstmtInsert.executeUpdate();
+
+        ResultSet generatedKeys = pstmtInsert.getGeneratedKeys();
+        if (generatedKeys.next()) {
+            resp.getWriter().println("Id: "+generatedKeys.getLong(1));
+        }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            resp.getWriter().println("Error "+e.getMessage());
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
